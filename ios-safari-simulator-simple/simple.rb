0f6b6d2e0173d9b7931d9c052343049059cc1cbc
Steven Miller <s_c_m_jr@yahoo.com>
2015-05-23 01:41:03 -0700

    Updated with Appium/Selenium description
    Updated with better approach and comments

--- ios-safari-simulator-simple/simple.rb
+++ ios-safari-simulator-simple/simple.rb
@@ -12,28 +12,37 @@ desired_caps = {
    }  
   } 
 
-# Create a new Appium driver
-@driver = Appium::Driver.new(desired_caps).start_driver 
+# Create a new Appium specific driver with helpers availabe
+@appium_driver = Appium::Driver.new(desired_caps)
+
+# Standard Selenium driver without any Appium methods.
+# Need to convert to Selenium driver to make "get" call
+# since Appium doesn't support "get" method.
+@selenium_driver = @appium_driver.start_driver
 
 # Promote appium method to class instance methods
-Appium.promote_appium_methods Object  
+# Without promoting we would need to make all calls with the @appium_driver, example:
+#   @appium_driver.find_element(:id, 'lst-ib')
+# After promoting to a class instance method we can the method directly, example:
+#   find_element(:id, 'lst-ib')
+Appium.promote_appium_methods Object
 
 # Open web page
 # Note apple.com open first unless you override it in caps using safariInitialUrl
-@driver.get("http://www.google.com/")  
+@selenium_driver.get("http://www.google.com/")
 
 # Extra time to allow webpage to load
 sleep(5)
 
 # Find Search Box element, click on it, type in Search Query 
-element = @driver.find_element(:id, 'lst-ib')
+element = find_element(:id, 'lst-ib')
 element.click
 element.send_keys 'Steven Miller Dentedghost Appium'
 # Extra pause for demostration
 sleep(2)
 
 # Find Search Button element, click on it
-element = @driver.find_element(:id, 'tsbb')
+element = find_element(:id, 'tsbb')
 element.click
 
 # Extra time to allow webpage to load
