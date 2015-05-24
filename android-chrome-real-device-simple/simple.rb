bc6267617af00f1205b31f6a46c64be0b3bebbbd
Steven Miller <s_c_m_jr@yahoo.com>
2015-05-24 01:21:20 -0700

    Appium on Real Android Device with Chrome Browser (Simple Introduction)
    Step by Step instruction available at
    http://dentedghost.blogspot.com/2015/05/appium-on-real-android-device-wi
    th.html

---
+++ android-chrome-real-device-simple/simple.rb
@@ -0,0 +1,56 @@
+# Include the needed gems
+require 'rubygems'
+require 'appium_lib'
+
+# Configure call to Appium Server
+# More information is available at http://appium.io/slate/en/master/?ruby#appium-server-capabilities.
+desired_caps = {
+   caps:  {
+        platformName:    'Android',
+        platformVersion:   '4.4',
+        deviceName:      'TA96515UMI',
+        browserName:     'Chrome',
+    }
+}
+
+# Create a new Appium specific driver with helpers availabe
+@appium_driver = Appium::Driver.new(desired_caps)
+
+# Standard Selenium driver without any Appium methods.
+# Need to convert to Selenium driver to make "get" call
+# since Appium doesn't support "get" method.
+@selenium_driver = @appium_driver.start_driver
+
+# Promote appium method to class instance methods
+# Without promoting we would need to make all calls with the @appium_driver, example:
+#   @appium_driver.find_element(:id, 'lst-ib')
+# After promoting to a class instance method we can the method directly, example:
+#   find_element(:id, 'lst-ib')
+Appium.promote_appium_methods Object
+
+# Open web page
+@selenium_driver.get("http://www.google.com/")
+
+# Extra time to allow webpage to load
+sleep(5)
+
+# Find Search Box element, click on it, type in Search Query
+element = find_element(:id, 'lst-ib')
+element.click
+element.send_keys 'Steven Miller Dentedghost Appium'
+
+# Extra pause for demostration
+sleep(2)
+
+# Find Search Button element, click on it
+element = find_element(:id, 'tsbb')
+element.click
+
+# Extra time to allow webpage to load
+sleep (5)
+
+# Properly close down the driver
+driver_quit
+
+# Print test pass success message
+puts "Tests Succeeded"
